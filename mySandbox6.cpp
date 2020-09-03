a14f5d47c14784ee80be12700adbe9ce7fe6c874
Jawwad Asghar <jawwad.asghar@gmail.com>
2020-09-02 22:24:20 -0400

    Correcting out folder in gitignore. Adding in CMakeLists.txt and mySandbox6 initial commit.

---
+++ mySandbox6.cpp
@@ -0,0 +1,13 @@
+﻿// mySandbox5.cpp : Defines the entry point for the application.
+//
+
+#include "mySandbox6.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Cmake in the house" << endl;
+	return 0;
+}
+//
